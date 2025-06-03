62141679d54a7b5df615caa8577b5d2d0a62c984
Ray Hagimoto <ray.m.hagimoto@gmail.com>
2025-06-03 00:10:29 -0500

    dirty commit

--- content/gallery/index.md
+++ content/gallery/index.md
@@ -2,5 +2,4 @@
 title: 'photography'
 date: '2025-05-30T17:15:10-05:00'
 draft: false
-full_width_gallery: true
 ---
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    screens: {
+      md: "900px",    // instead of 853px
+      lg: "1100px",   // instead of 1024px
+      // …etc…
+    },
+  },
+  // …
+};
